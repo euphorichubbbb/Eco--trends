3a1e6a66cfb60bf5d77a991215a1713d6705444f
euphorichubbbb <106897467+euphorichubbbb@users.noreply.github.com>
2023-03-19 21:56:43 +0530

    config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from configparser import ConfigParser
 
 
 def config(filename='database.ini', section='postgresql'):
-    # create a parser
+    # create a parser)
     parser = ConfigParser()
     # read config file
     parser.read(filename)
